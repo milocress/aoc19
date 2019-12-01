ff9d91b9afd339d7bcfdd4711b531c10c7c655dd
Milo Cress <cress@mit.edu>
2019-12-01 00:26:53 -0500

    Day 1

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
-import Lib
-
 main :: IO ()
-main = someFunc
+main = do
+  nums <- fmap read . lines <$> readFile "res/day1.txt"
+  print . sum $ fuel <$> nums
+  print . sum $ allFuel' <$> nums
+
+fuel :: Int -> Int
+fuel = (+ (-2)) . (floor :: Double -> Int) . (/ 3) . fromIntegral
+
+allFuel :: Int -> Int
+allFuel mass = case compare (fuel mass) 0 of
+  LT -> mass
+  EQ -> mass
+  GT -> mass + allFuel (fuel mass)
+
+allFuel' = allFuel . fuel
